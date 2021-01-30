34fce98a07c48bb59f137ef9943e77f7d61f649d
Ricky Morgan <student+1827@codingwithkids.com>
2021-01-30 23:04:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var square = document.getElementById("box"); 
+var width = 100px;
+var height = 100px;
+
+
+function grow() {
+  square.style.width = "200px";
+  square.style.height = "200px";
+}
+
+function shrink() {
+  square.style.width = "50px";
+  square.style.height = "50px";
+}
